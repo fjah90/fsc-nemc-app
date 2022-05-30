b37fe64926e12fb297d4cf67fd8425372ce6d207
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-30 06:58:45 -0400

    feat: add Home view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './views';
 
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
-
+      <Home />
     </ThemeProvider>
   );
 }
