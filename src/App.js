a89b5a795e0938f1b5eadfad4c58f937a13f9a98
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-30 06:49:00 -0400

    feat: add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
-    <ThemeProvider
-      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-    >
-      <div className="App">
-        <header className="App-header"></header>
-      </div>
+    <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
+
     </ThemeProvider>
   );
 }
