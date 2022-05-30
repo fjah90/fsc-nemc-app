f168b106d56287f06fb1ff6c39cde1db4fe73201
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-30 15:08:58 -0400

    chrore: set node.env production

--- src/infrastructure/services/api/files/index.js
+++ src/infrastructure/services/api/files/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const apiURL = process.NODE_ENV === 'staging' ? 'https://fsc-nemc-api.herokuapp.com/api/v1' : 'http://localhost:4000/api/v1';
+const apiURL = process.NODE_ENV === 'production' ? 'https://fsc-nemc-api.herokuapp.com/api/v1' : 'http://localhost:4000/api/v1';
 
 export default {
     getAll: async () => {
+        console.info(apiURL)
         const response = await axios.get(apiURL +'/files/data');
 
         return response.data
